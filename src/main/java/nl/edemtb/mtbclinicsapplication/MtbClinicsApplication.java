10762fe1d23c01525213254da32ac231235da660
Christiaan83 <c.zielman@gmail.com>
2024-03-08 21:03:35 +0100

    opzetten applicatie

---
+++ src/main/java/nl/edemtb/mtbclinicsapplication/MtbClinicsApplication.java
@@ -0,0 +1,13 @@
+package nl.edemtb.mtbclinicsapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtbClinicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MtbClinicsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/edemtb/mtbclinicsapplication/MtbClinicsApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.edemtb.mtbclinicsapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtbClinicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
