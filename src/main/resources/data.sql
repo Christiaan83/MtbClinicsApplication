2919847ffe2bdc91345a9af5a96bda95de26f3dd
Christiaan83 <c.zielman@gmail.com>
2024-04-03 15:02:25 +0200

    Loading Initial mountainbike data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,16 @@
+
+INSERT INTO mountainbikes (id, name, wheel_size, frame_size, gears, amount, pricePerDayPart, for_adult, full_suspension, availible )
+VALUES (1001, 'Hardtail small', ' 29 inch', 'small', 10, 10, 30, true, false, true),
+(1002,'Hardtail medium', ' 29 inch', 'medium', 10, 10, 30, true, false, true),
+(1003, 'Hardtail large', ' 29 inch', 'large', 10, 10, 30, true, false, true),
+(1004, 'Hardtail extra large', ' 29 inch', 'extra large', 10, 10, 30, true, false, true),
+(1005, 'Full Suspension small', ' 29 inch', 'small', 12, 5, 35, true, true, true),
+(1006,'Full Suspension medium', ' 29 inch', 'medium', 12, 5, 35, true, true, true),
+(1007, 'Full Suspension large', ' 29 inch', 'large', 12, 5, 35, true, true, true),
+(1008, 'Full Suspension extra large', ' 29 inch', 'extra large', 12, 5, 35, true, true, true),
+(1009, 'Kids 20 inch', ' 20 inch', 'small', 9, 5, 35, true, true, true),
+(1010, 'Kids 24 inch', ' 24 inch', 'large', 10, 5, 35, true, true, true);
+
+
+
+
