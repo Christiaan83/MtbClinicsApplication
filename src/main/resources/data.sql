947a2e15fb7fa64d62624de525d20a072f235594
Christiaan83 <c.zielman@gmail.com>
2024-04-05 11:29:48 +0200

    corrected spelling mistake

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 
-INSERT INTO mountainbikes (id, name, wheel_size, frame_size, gears, amount, pricePerDayPart, for_adult, full_suspension, availible )
+INSERT INTO mountainbikes (id, name, wheel_size, frame_size, gears, amount, price_per_day_part, for_adult, full_suspension, available)
 VALUES (1001, 'Hardtail small', ' 29 inch', 'small', 10, 10, 30, true, false, true),
 (1002,'Hardtail medium', ' 29 inch', 'medium', 10, 10, 30, true, false, true),
 (1003, 'Hardtail large', ' 29 inch', 'large', 10, 10, 30, true, false, true),
