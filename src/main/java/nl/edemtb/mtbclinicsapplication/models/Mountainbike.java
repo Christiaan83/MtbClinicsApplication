c575f899063cf38a89758178ac88feeb2bd1d63e
Christiaan83 <c.zielman@gmail.com>
2024-04-09 16:40:05 +0200

    add GenericGenerator

--- src/main/java/nl/edemtb/mtbclinicsapplication/models/Mountainbike.java
+++ src/main/java/nl/edemtb/mtbclinicsapplication/models/Mountainbike.java
@@ -1,9 +1,8 @@
 package nl.edemtb.mtbclinicsapplication.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 
 @Entity
 @Table(name = "mountainbikes")
@@ -11,7 +10,14 @@ import jakarta.persistence.Table;
 public class Mountainbike {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(generator = "sequence-generator")
+    @GenericGenerator(name = "sequence-generator",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {@Parameter(name = "sequence_name", value = "user_sequence"),
+                    @Parameter(name = "initial_value", value = "1011"),
+                    @Parameter(name = "increment_size", value = "1")
+            }
+    )
     private Long id;
 
     private String name;
