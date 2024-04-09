4e78eb043436b054f021517407a414f0b293f1cd
Christiaan83 <c.zielman@gmail.com>
2024-04-09 16:37:38 +0200

    update frame_size names

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,15 +1,14 @@
-
-INSERT INTO mountainbikes (id, name, wheel_size, frame_size, gears, amount, price_per_day_part, for_adult, full_suspension, available)
+INSERT INTO mountainbikes (id, name, wheel_size, frame_size, gears, amount, price_per_day_part, for_adult,full_suspension, available)
 VALUES (1001, 'Hardtail S', ' 29 inch', 'small', 10, 10, 30, true, false, true),
-(1002,'Hardtail M', ' 29 inch', 'medium', 10, 10, 30, true, false, true),
-(1003, 'Hardtail L', ' 29 inch', 'large', 10, 10, 30, true, false, true),
-(1004, 'Hardtail XL', ' 29 inch', 'extra large', 10, 10, 30, true, false, true),
-(1005, 'Full Suspension S', ' 29 inch', 'small', 12, 5, 35, true, true, true),
-(1006,'Full Suspension M', ' 29 inch', 'medium', 12, 5, 35, true, true, true),
-(1007, 'Full Suspension L', ' 29 inch', 'large', 12, 5, 35, true, true, true),
-(1008, 'Full Suspension XL', ' 29 inch', 'extra large', 12, 5, 35, true, true, true),
-(1009, 'Kids 20 inch', ' 20 inch', 'small', 9, 5, 35, true, true, true),
-(1010, 'Kids 24 inch', ' 24 inch', 'large', 10, 5, 35, true, true, true);
+       (1002, 'Hardtail M', ' 29 inch', 'medium', 10, 10, 30, true, false, true),
+       (1003, 'Hardtail L', ' 29 inch', 'large', 10, 10, 30, true, false, true),
+       (1004, 'Hardtail XL', ' 29 inch', 'extra-large', 10, 10, 30, true, false, true),
+       (1005, 'Full Suspension S', ' 29 inch', 'small', 12, 5, 35, true, true, true),
+       (1006, 'Full Suspension M', ' 29 inch', 'medium', 12, 5, 35, true, true, true),
+       (1007, 'Full Suspension L', ' 29 inch', 'large', 12, 5, 35, true, true, true),
+       (1008, 'Full Suspension XL', ' 29 inch', 'extra-large', 12, 5, 35, true, true, true),
+       (1009, 'Kids 20 inch', ' 20 inch', 'small', 9, 5, 35, false, true, true),
+       (1010, 'Kids 24 inch', ' 24 inch', 'large', 10, 5, 35, false, true, true);
 
 
 
