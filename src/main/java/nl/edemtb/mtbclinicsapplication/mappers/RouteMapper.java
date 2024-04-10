1f8b1ff7730628a7823ee72680bd6cc7d86786dd
Christiaan83 <c.zielman@gmail.com>
2024-04-10 11:00:57 +0200

    add dto mappers

---
+++ src/main/java/nl/edemtb/mtbclinicsapplication/mappers/RouteMapper.java
@@ -0,0 +1,45 @@
+package nl.edemtb.mtbclinicsapplication.mappers;
+
+import nl.edemtb.mtbclinicsapplication.dtos.RouteDto;
+import nl.edemtb.mtbclinicsapplication.dtos.RouteInputDto;
+import nl.edemtb.mtbclinicsapplication.models.Route;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RouteMapper {
+
+    public Route transferToRoute(RouteInputDto dto) {
+        if (dto == null) {
+            return null;
+        }
+        var route = new Route();
+        route.setName(dto.getName());
+        route.setRouteType(dto.getRouteType());
+        route.setDifficulty(dto.getDifficulty());
+        route.setStartingPoint(dto.getStartingPoint());
+        route.setPlace(dto.getPlace());
+        route.setProvince(dto.getProvince());
+        route.setRouteInformation(dto.getRouteInformation());
+        route.setDistance(dto.getDistance());
+        route.setAvailable(dto.getAvailable());
+        return route;
+    }
+
+    public RouteDto transferToDto(Route route) {
+        if (route == null) {
+            return null;
+        }
+        RouteDto dto = new RouteDto();
+        dto.setId(route.getId());
+        dto.setName(route.getName());
+        dto.setRouteType(route.getRouteType());
+        dto.setDifficulty(route.getDifficulty());
+        dto.setStartingPoint(route.getStartingPoint());
+        dto.setPlace(route.getPlace());
+        dto.setProvince(route.getProvince());
+        dto.setRouteInformation(route.getRouteInformation());
+        dto.setDistance(route.getDistance());
+        dto.setAvailable(route.getAvailable());
+        return dto;
+    }
+}
