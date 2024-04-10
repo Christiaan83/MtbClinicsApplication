4ef4326a7441ef3cebfedbbd10ec7f1b32c9c50c
Christiaan83 <c.zielman@gmail.com>
2024-04-10 15:02:53 +0200

    add update, delete and add methods

--- src/main/java/nl/edemtb/mtbclinicsapplication/services/RoutesService.java
+++ src/main/java/nl/edemtb/mtbclinicsapplication/services/RoutesService.java
@@ -1,12 +1,14 @@
 package nl.edemtb.mtbclinicsapplication.services;
 
 import nl.edemtb.mtbclinicsapplication.dtos.RouteDto;
+import nl.edemtb.mtbclinicsapplication.dtos.RouteInputDto;
 import nl.edemtb.mtbclinicsapplication.exceptions.RecordNotFoundException;
 import nl.edemtb.mtbclinicsapplication.helpers.RouteHelper;
 import nl.edemtb.mtbclinicsapplication.mappers.RouteMapper;
 import nl.edemtb.mtbclinicsapplication.models.Route;
 import nl.edemtb.mtbclinicsapplication.repositories.RouteRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.Optional;
@@ -36,7 +38,7 @@ public class RoutesService {
             Route route = routeOptional.get();
             return routeMapper.transferToDto(route);
         } else {
-            throw new RecordNotFoundException("Route Niet gevonden.");
+            throw new RecordNotFoundException("Route niet met id: " + id +" niet gevonden.");
         }
     }
 
@@ -56,5 +58,27 @@ public class RoutesService {
         RouteHelper.checkRouteAvailability(available);
         routeList = routeRepository.findRoutesByDifficultyAndAvailable(difficulty, true);
         return routeMapper.transferRouteListToDtoList(routeList);
+    }
+
+    public RouteDto addRoute(RouteInputDto dto) {
+        Route route = routeMapper.transferToRoute(dto);
+        routeRepository.save(route);
+
+        return routeMapper.transferToDto(route);
+    }
+
+    public void deleteRoute(@RequestBody Long id) {
+
+        if (routeRepository.findById(id).isPresent()){
+            routeRepository.deleteById(id);
+        }else {
+            throw new RecordNotFoundException("Geen route gevonden met id: " + id);
+        }
+    }
+    public void updateRoute(Long id, RouteInputDto inputDto){
+        if(routeRepository.findById(id).isEmpty()){
+            throw new RecordNotFoundException("Geen route gevonden!");
+        }
+        routeMapper.routeInputMapper(id, inputDto);
     }
 }
