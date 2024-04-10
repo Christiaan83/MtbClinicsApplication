8d7449e9a21810d5575d9097c3792894f070a50d
Christiaan83 <c.zielman@gmail.com>
2024-04-10 10:08:57 +0200

    add dto and inputDto

--- src/main/java/nl/edemtb/mtbclinicsapplication/dtos/RouteDto.java
+++ src/main/java/nl/edemtb/mtbclinicsapplication/dtos/RouteDto.java
@@ -1,2 +1,95 @@
-package nl.edemtb.mtbclinicsapplication.dtos;public class RouteDto {
+package nl.edemtb.mtbclinicsapplication.dtos;
+
+public class RouteDto {
+
+    private Long id;
+    private String name;
+    private String routeType;
+    private String difficulty;
+    private String startingPoint;
+    private String place;
+    private String province;
+    private String routeInformation;
+    private Double distance;
+    private Boolean available;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRouteType() {
+        return routeType;
+    }
+
+    public void setRouteType(String routeType) {
+        this.routeType = routeType;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public String getStartingPoint() {
+        return startingPoint;
+    }
+
+    public void setStartingPoint(String startingPoint) {
+        this.startingPoint = startingPoint;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getRouteInformation() {
+        return routeInformation;
+    }
+
+    public void setRouteInformation(String routeInformation) {
+        this.routeInformation = routeInformation;
+    }
+
+    public Double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(Double distance) {
+        this.distance = distance;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
 }
