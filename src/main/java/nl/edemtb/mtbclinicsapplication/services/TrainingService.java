c743ce7771660a1937f974f355e9090c2ab8cda0
Christiaan83 <c.zielman@gmail.com>
2024-04-12 10:59:48 +0200

    put logic into the service

--- src/main/java/nl/edemtb/mtbclinicsapplication/services/TrainingService.java
+++ src/main/java/nl/edemtb/mtbclinicsapplication/services/TrainingService.java
@@ -1,2 +1,63 @@
-package nl.edemtb.mtbclinicsapplication.services;public class TrainingService {
+package nl.edemtb.mtbclinicsapplication.services;
+
+import nl.edemtb.mtbclinicsapplication.dtos.TrainingDto;
+import nl.edemtb.mtbclinicsapplication.dtos.TrainingInpuDto;
+import nl.edemtb.mtbclinicsapplication.exceptions.RecordNotFoundException;
+import nl.edemtb.mtbclinicsapplication.mappers.RouteMapper;
+import nl.edemtb.mtbclinicsapplication.mappers.TrainingMapper;
+import nl.edemtb.mtbclinicsapplication.models.Training;
+import nl.edemtb.mtbclinicsapplication.repositories.TrainingRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TrainingService {
+
+
+    private final TrainingRepository trainingRepository;
+    private final TrainingMapper trainingMapper;
+
+    public TrainingService(TrainingRepository trainingRepository, TrainingMapper trainingMapper) {
+        this.trainingRepository = trainingRepository;
+        this.trainingMapper = trainingMapper;
+    }
+
+    public List<TrainingDto> getAllTrainings() {
+        List<Training> trainings = trainingRepository.findAll();
+        return trainingMapper.transferTrainingListToDto(trainings);
+    }
+
+    public TrainingDto getTrainingById(Long id) {
+        Optional<Training> trainingOptional = trainingRepository.findById(id);
+
+        if (trainingOptional.isPresent()) {
+            Training training = trainingOptional.get();
+            return trainingMapper.transferToDto(training);
+        } else {
+            throw new RecordNotFoundException("Training met id " + id + " niet gevonden!");
+        }
+    }
+
+    public TrainingDto addTraining(TrainingDto dto) {
+
+        Training training = trainingMapper.transferToTraining(dto);
+        trainingRepository.save(training);
+
+        return trainingMapper.transferToDto(training);
+    }
+
+    public void deleteTraining(Long id) {
+        if (trainingRepository.findById(id).isPresent()) trainingRepository.deleteById(id);
+    }
+
+    public TrainingDto updateTraining(Long id, TrainingInpuDto inputDto) {
+
+        if (trainingRepository.findById(id).isPresent()) {
+            return trainingMapper.trainingInputMapper(id, inputDto);
+        } else {
+            return null;
+        }
+    }
 }
