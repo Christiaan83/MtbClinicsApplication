beb7585bc04cd154901a8ee81f73883e8692afa5
Christiaan83 <c.zielman@gmail.com>
2024-04-15 14:32:35 +0200

    add dtos and repositories

---
+++ src/main/java/nl/edemtb/mtbclinicsapplication/dtos/ContactFormDto.java
@@ -0,0 +1,11 @@
+package nl.edemtb.mtbclinicsapplication.dtos;
+
+public class ContactFormDto {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Integer mobileNumber;
+    private String message;
+}
