eb8ed02cc1cc74d6074d2d22b92a3b9687ef2ac7
Christiaan83 <c.zielman@gmail.com>
2024-04-15 12:40:41 +0200

    add unregisteredUser dto en repository

--- src/main/java/nl/edemtb/mtbclinicsapplication/repositories/UnregisteredUserRepository.java
+++ src/main/java/nl/edemtb/mtbclinicsapplication/repositories/UnregisteredUserRepository.java
@@ -1,7 +1,8 @@
 package nl.edemtb.mtbclinicsapplication.repositories;
 
+
 import nl.edemtb.mtbclinicsapplication.models.UnregisteredUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<UnregisteredUser, Long> {
+public interface UnregisteredUserRepository extends JpaRepository<UnregisteredUser, Long> {
 }
