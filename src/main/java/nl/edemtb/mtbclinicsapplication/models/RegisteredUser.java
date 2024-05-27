a86df449c793ce9405ec1bfcb7f14fbdf471008c
Christiaan83 <c.zielman@gmail.com>
2024-05-27 12:40:17 +0200

    extends basicUser and add id's

--- src/main/java/nl/edemtb/mtbclinicsapplication/models/RegisteredUser.java
+++ src/main/java/nl/edemtb/mtbclinicsapplication/models/RegisteredUser.java
@@ -9,23 +9,13 @@ import jakarta.validation.constraints.Pattern;
 
 @Entity
 @Table(name = "registered_users")
-public class RegisteredUser {
+public class RegisteredUser extends BasicUser{
 
     @Id
     @Column(nullable = false, unique = true)
     private String username;
     @Column(nullable = false, unique = true)
     private String password;
-    @Column
-    private String firstName;
-    @Column
-    private String lastName;
-    @Column
-    @Email
-    private String email;
-    @Column
-    @Pattern(regexp = "^\\(?([+]31|0031|0)-?6(\\s?|-)([0-9]\\s{0,3}){8}$")
-    private Integer mobileNumber;
     @Column
     private String userRole;
     @Column(nullable = false)
@@ -45,38 +35,6 @@ public class RegisteredUser {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Integer getMobileNumber() {
-        return mobileNumber;
-    }
-
-    public void setMobileNumber(Integer mobileNumber) {
-        this.mobileNumber = mobileNumber;
     }
 
     public String getUserRole() {
--- src/main/java/nl/edemtb/mtbclinicsapplication/models/UnregisteredUser.java
+++ src/main/java/nl/edemtb/mtbclinicsapplication/models/UnregisteredUser.java
@@ -1,19 +1,33 @@
 package nl.edemtb.mtbclinicsapplication.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.util.List;
 
 @Entity
 @Table(name = "unregistered_users")
 public class UnregisteredUser extends BasicUser{
+    @Id
+    @GeneratedValue(generator = "sequence-generator4")
+    @GenericGenerator(name = "sequence-generator4",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {@org.hibernate.annotations.Parameter(name = "sequence_user", value = "route_user"),
+                    @org.hibernate.annotations.Parameter(name = "initial_value", value = "1002"),
+                    @org.hibernate.annotations.Parameter(name = "increment_size", value = "1")
+            }
+    )
+    private Long id;
 
     @OneToMany(mappedBy = "unregisteredUser")
     private List<Rental> rentals;
 
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
     public List<Rental> getRentals() {
         return rentals;
     }
