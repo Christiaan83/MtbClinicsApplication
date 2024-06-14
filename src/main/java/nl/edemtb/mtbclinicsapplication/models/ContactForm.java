f0e183e09f58cb9914b601f0298c08558f8652bd
Christiaan83 <c.zielman@gmail.com>
2024-06-14 09:28:41 +0200

    edit Id settings
    This reverts commit 661571a0ce41fd32ef1ba3c5837f9b842dc06385.

--- src/main/java/nl/edemtb/mtbclinicsapplication/models/ContactForm.java
+++ src/main/java/nl/edemtb/mtbclinicsapplication/models/ContactForm.java
@@ -1,22 +1,13 @@
 package nl.edemtb.mtbclinicsapplication.models;
 
 import jakarta.persistence.*;
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Parameter;
 
 @Entity
 @Table(name = "contact_forms")
 public class ContactForm extends BasicUser {
 
     @Id
-    @GeneratedValue(generator = "sequence-generator6")
-    @GenericGenerator(name = "sequence-generator6",
-            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
-            parameters = {@Parameter(name = "sequence_form", value = "route_form"),
-                    @Parameter(name = "initial_value", value = "1003"),
-                    @Parameter(name = "increment_size", value = "1")
-            }
-    )
+    @GeneratedValue
     private Long id;
     @Column(length = Integer.MAX_VALUE)
     private String message;
