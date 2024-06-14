661571a0ce41fd32ef1ba3c5837f9b842dc06385
Christiaan83 <c.zielman@gmail.com>
2024-06-14 09:24:06 +0200

    edit Id settings and add message

--- src/main/java/nl/edemtb/mtbclinicsapplication/models/ContactForm.java
+++ src/main/java/nl/edemtb/mtbclinicsapplication/models/ContactForm.java
@@ -1,13 +1,22 @@
 package nl.edemtb.mtbclinicsapplication.models;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 
 @Entity
 @Table(name = "contact_forms")
 public class ContactForm extends BasicUser {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(generator = "sequence-generator6")
+    @GenericGenerator(name = "sequence-generator6",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {@Parameter(name = "sequence_form", value = "route_form"),
+                    @Parameter(name = "initial_value", value = "1003"),
+                    @Parameter(name = "increment_size", value = "1")
+            }
+    )
     private Long id;
     @Column(length = Integer.MAX_VALUE)
     private String message;
